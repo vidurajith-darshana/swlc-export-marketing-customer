1e393ec8f3b410037ca416d35f91f4c2de6a5418
Dulshan Ranaweera <dulshan.ranaweera@neosolax.com>
2021-07-07 22:57:50 +0530

    login after rederect to home

--- src/app/pages/service/customer-web-services/customer-login.service.ts
+++ src/app/pages/service/customer-web-services/customer-login.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {AlertService} from '../../_alert';
+import {Router, ActivatedRoute, ParamMap} from '@angular/router';
 
 @Injectable({
     providedIn: 'root'
@@ -15,31 +16,38 @@ export class CustomerLoginService {
 
     constructor(
         protected alertService: AlertService,
-        private httpClient: HttpClient
+        private httpClient: HttpClient,
+        private router: Router,
+        private route: ActivatedRoute
     ) {
     }
 
     public customerLogin(userName, userPassword) {
 
-        let params = new URLSearchParams();
+        const params = new URLSearchParams();
         params.append('username', userName);
         params.append('password', userPassword);
         params.append('grant_type', 'password');
 
-        let headers =
+        const headers =
             new HttpHeaders({
                 'Authorization': 'Basic dXNlcjo=',
                 'Content-Type': 'application/x-www-form-urlencoded'
             });
 
-        let url = `${this.APP_URL + 'api/v1/authorize'}`;
+        const url = `${this.APP_URL + 'api/v1/authorize'}`;
 
         this.httpClient.post(url, params.toString(), {headers: headers}).subscribe((data) => {
                 this.saveToken(data);
                 this._getUserDetails(userName);
+                this.router.navigate(['/heroes']);
             }
-        ,
-            err => this.alertService.warn('Invalid Credentials', this.options));
+            ,
+            err => (
+                this.alertService.warn('Invalid Credentials', this.options)
+                // this.router.navigate(['/heroes'])
+            )
+        );
 
 
     }
@@ -47,8 +55,8 @@ export class CustomerLoginService {
     public saveToken(data) {
         // window.sessionStorage.setItem('token', );
         localStorage.setItem('token', JSON.stringify(data));
-        let itemList = new Array();
-        localStorage.setItem("itemList",JSON.stringify(itemList));
+        const itemList = new Array();
+        localStorage.setItem('itemList', JSON.stringify(itemList));
     }
 
     public checkCredentials() {
@@ -61,12 +69,12 @@ export class CustomerLoginService {
         window.location.reload();
     }
 
-    public _getUserDetails(customerEmail){
-        let url = `${this.APP_URL + 'api/v1/user/getDetails/'+customerEmail}`;
-        this.httpClient.get(url).subscribe((data:[]) => {
+    public _getUserDetails(customerEmail) {
+        const url = `${this.APP_URL + 'api/v1/user/getDetails/' + customerEmail}`;
+        this.httpClient.get(url).subscribe((data: []) => {
             console.log(data['body'].id);
-            localStorage.setItem('loggedUserId',data['body'].id);
-        },error => {
+            localStorage.setItem('loggedUserId', data['body'].id);
+        }, error => {
 
         });
     }
