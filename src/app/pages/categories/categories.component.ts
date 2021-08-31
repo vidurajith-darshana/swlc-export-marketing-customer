746b15661b7ce2216d18ab7aa9c0deadc2c6b9b1
Dulshan Ranaweera <dulshan.ranaweera@neosolax.com>
2021-08-31 21:23:05 +0530

    promotions are added to home page

--- src/app/pages/categories/categories.component.ts
+++ src/app/pages/categories/categories.component.ts
@@ -1,10 +1,12 @@
 import {Component, OnInit, VERSION} from '@angular/core';
 import {Category} from '../model/category';
 import {CategoryService} from '../service/customer-web-services/category.service';
+import {PromotionService} from '../service/customer-web-services/promotion.service';
 import {ProductService} from "../service/customer-web-services/product.service";
 import {Product} from "../model/product";
 import {AlertService} from "../_alert";
 import {Router} from "@angular/router";
+import {Promotion} from '../model/promotion';
 
 @Component({
     selector: 'app-categories',
@@ -16,6 +18,9 @@ export class CategoriesComponent implements OnInit {
     categoryName = 'Angular ' + VERSION.major;
 
     private categoryList: Category[];
+    private promotionList: Promotion[];
+    private promotionListf: Promotion[];
+    private promotionLista: Promotion[];
 
     private productList : Product[];
 
@@ -30,6 +35,7 @@ export class CategoriesComponent implements OnInit {
     constructor(
         protected alertService: AlertService,
         private categoryService: CategoryService,
+        private promotionService: PromotionService,
         private productService : ProductService,
         private router : Router
         ) {
@@ -37,9 +43,27 @@ export class CategoriesComponent implements OnInit {
 
     ngOnInit(): void {
         this.getAllCategoryList();
+        this.getAllPromotionList();
         this.getProductList(0);
     }
 
+    private getAllPromotionList() {
+            this.promotionService.getAllPromotions().subscribe(
+            (data: Object[]) => {
+
+                // this.promotionLista = data['body'].content[0];
+                this.promotionListf = data['body'].content[0];
+                data['body'].content.shift()
+                this.promotionList = data['body'].content;
+                console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+                console.log(data);
+                console.log(this.promotionListf);
+                console.log(this.promotionList);
+                console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+            },
+            error => {
+            });
+    }
     private getAllCategoryList() {
         this.categoryService.getAllCategory().subscribe(
             (data: Object[]) => {
@@ -60,28 +84,14 @@ export class CategoriesComponent implements OnInit {
     }
 
     private _addToCartByHome(itemId,itemName,itemImage,itemPrice){
-        if (this.quantity > 0){
-            try {
-                let subTotal = itemPrice * this.quantity;
-                let list = JSON.parse(localStorage.getItem('itemList'));
-                if (list.length > 0){
-                    let find = list.find(name => name.itemId === itemId);
-                    console.log(find)
-                    if (find === undefined){
-                        let item = {
-                            itemId: itemId,
-                            itemName: itemName,
-                            itemImage: itemImage,
-                            itemPrice: itemPrice,
-                            itemQty: this.quantity,
-                            subTotal: subTotal
-                        };
-                        list.push(item);
-                    }else{
-                        find.itemQty += this.quantity;
-                        find.subTotal += subTotal;
-                    }
-                }else{
+    if (this.quantity > 0){
+        try {
+            let subTotal = itemPrice * this.quantity;
+            let list = JSON.parse(localStorage.getItem('itemList'));
+            if (list.length > 0){
+                let find = list.find(name => name.itemId === itemId);
+                console.log(find)
+                if (find === undefined){
                     let item = {
                         itemId: itemId,
                         itemName: itemName,
@@ -91,15 +101,29 @@ export class CategoriesComponent implements OnInit {
                         subTotal: subTotal
                     };
                     list.push(item);
+                }else{
+                    find.itemQty += this.quantity;
+                    find.subTotal += subTotal;
                 }
-
-                localStorage.setItem('itemList', JSON.stringify(list));
-                this.alertService.success(itemName + 'added to cart', this.options);
-            } catch (e) {
-                this.alertService.warn('Something went wrong', this.options);
+            }else{
+                let item = {
+                    itemId: itemId,
+                    itemName: itemName,
+                    itemImage: itemImage,
+                    itemPrice: itemPrice,
+                    itemQty: this.quantity,
+                    subTotal: subTotal
+                };
+                list.push(item);
             }
-        }else{
-            this.alertService.warn('Quantity must be greater than 0', this.options);
+
+            localStorage.setItem('itemList', JSON.stringify(list));
+            this.alertService.success(itemName + 'added to cart', this.options);
+        } catch (e) {
+            this.alertService.warn('Something went wrong', this.options);
+        }
+    }else{
+        this.alertService.warn('Quantity must be greater than 0', this.options);
         }
     }
 
---
+++ src/app/pages/model/promotion.ts
@@ -0,0 +1,8 @@
+export class Promotion {
+    id: number;
+    status: string;
+    image: string;
+    description: string;
+    heading: string;
+    createDate: string;
+}
